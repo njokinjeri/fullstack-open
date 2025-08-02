da876ed711fcc68036bb0caba95dde7cfa40ecab
NjokiNjeri <njerinjoki22@gmail.com>
2025-08-02 23:51:55 +0300

    phonebook hook added

--- part-2/phonebook/src/main.jsx
+++ part-2/phonebook/src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import axios from 'axios'
+
+const promise = axios.get('http://localhost:3001/persons')
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
