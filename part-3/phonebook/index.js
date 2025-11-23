ee024a1c42da33b1f02b966ec0a80b7ebb20d669
njoki <njerinjoki22@gmail.com>
2025-11-23 09:27:00 +0300

    phonebook backend nodejs

---
+++ part-3/phonebook/index.js
@@ -0,0 +1,110 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+morgan.token('postData', (request) => {
+    if (request.method === 'POST') return ' ' + JSON.stringify(request.body);
+    else return ' ';
+});
+
+app.use(morgan(
+    ':method :url :status :res[content-length] - :response-time ms :postData'
+));
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons);
+})
+
+app.get('/info', (req, res) => {
+    const requestTime = new Date();
+    res.send(
+        `Phonebook has info for ${persons.length} people<br>
+        ${requestTime.toString()}
+        `);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id;
+    const person = persons.find(person => person.id === id);
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end();
+    }
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = req.params.id;
+    persons = persons.filter(person => person.id !== id);
+
+    res.status(204).end();
+});
+
+const generateId = () => {
+    const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => Number(n.id))) 
+    : 0;
+    return String(maxId + 1);
+} 
+
+app.post('/api/persons', (req, res) => {
+    const {name, number} = req.body;
+
+    if (!name) {
+        return res.status(400).json({
+            error: 'name is missing'
+        });
+    } 
+
+    if (!number) {
+        return res.status(400).json({
+            error: 'number is missing'
+        });
+    }
+
+    if (persons.find(p => p.name.toLowerCase() === name.toLowerCase())) { 
+        return res.status(400).json({
+            error: 'name must be unique'
+        });
+    }
+
+    const newPerson = {
+        id: generateId(),
+        name,
+        number,
+    } ;
+    persons.push(newPerson);
+
+    res.json(newPerson);
+});
+
+const PORT = process.env. PORT || 3001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
