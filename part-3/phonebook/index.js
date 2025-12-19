705c60e3e8b09e926d90cbd842d32fcc2d218b06
njoki <njerinjoki22@gmail.com>
2025-12-19 09:13:36 +0300

    enable cors for frontend

--- part-3/phonebook/index.js
+++ part-3/phonebook/index.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 
 const url = process.env.MONGODB_URI;
 
