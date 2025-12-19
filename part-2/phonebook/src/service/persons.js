04fd6284f93ca6ddffd52b0889874f91cb747788
njoki <njerinjoki22@gmail.com>
2025-12-19 09:02:17 +0300

    trigger redeploy

--- part-2/phonebook/src/service/persons.js
+++ part-2/phonebook/src/service/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseurl = import.meta.env.VITE_API_URL + 'api/persons';
+const baseurl = import.meta.env.VITE_API_URL + '/api/persons';
 
 const getAll = () => {
     return axios.get(baseurl).then(response => response.data);
